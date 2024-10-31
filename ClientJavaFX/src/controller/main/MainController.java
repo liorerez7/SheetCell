e6388503255dbd11be8f86764914b87333638fab
Lior <liorer@mta.ac.il>
2024-10-31 11:29:41 +0200

    now the updates are also consider the user

--- ClientJavaFX/src/controller/main/MainController.java
+++ ClientJavaFX/src/controller/main/MainController.java
@@ -75,7 +75,7 @@ public class MainController {
     private Map<Integer, UpdateCellInfo> versionToCellInfo = new HashMap<>();
 
 
-    public static final int REFRESH_INTERVAL = 1500;
+    public static final int REFRESH_INTERVAL = 500;
     public static final int INITIAL_DELAY = 0;
 
     public void setStage(Stage stage) {
